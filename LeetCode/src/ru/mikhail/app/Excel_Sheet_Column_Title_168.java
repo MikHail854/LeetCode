a3dee2c6d4bef61b1a0c72599b24ca169397bfb6
Milhail <vip.moneta95@mail.ru>
2020-01-26 00:38:56 +0700

    Excel Sheet Column Title - finished

--- LeetCode/src/ru/mikhail/app/Excel_Sheet_Column_Title_168.java
+++ LeetCode/src/ru/mikhail/app/Excel_Sheet_Column_Title_168.java
@@ -1,49 +1,42 @@
 package ru.mikhail.app;
 
+/**
+ * Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+ *
+ * For example:
+ *     1 -> A
+ *     2 -> B
+ *     3 -> C
+ *     ...
+ *     26 -> Z
+ *     27 -> AA
+ *     28 -> AB
+ *     ...
+ * Example 1:
+ * Input: 1
+ * Output: "A"
+
+ * Example 2:
+ * Input: 28
+ * Output: "AB"
+
+ * Example 3:
+ * Input: 701
+ * Output: "ZY"
+ */
 public class Excel_Sheet_Column_Title_168 {
     public String convertToTitle(int n) {
-
-        String[] symbol = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
         String result = "";
-        int helper;
+        if (n <= 0)
+            return result;
         while (n > 0) {
-            if (n < 27) {
-                result += symbol[n - 1];
-                return result;
-            } else {
-                helper = n / 26;
-                if (n % 26 == 0) {
-                    helper--;
-                }
-
-                if (helper == 27) {
-                    result += symbol[0];
-                    n -= 26 * 26;
-                }
-
-                if (helper < 27) {
-                    result = symbol[helper - 1] + result;
-                    n -= 26 * helper;
-                }
-                if (helper > 27) {
-                    result = symbol[helper - 1 - 26] + result;
-                    n -= 26 * helper - 26;
-                }
-            }
+            n--;
+            int last = n % 26;
+            result = ((char) (last + 65)) + result;
+            n = n / 26;
         }
         return result;
     }
-//        String sol = "";
-//        if(n <= 0)
-//            return sol;
-//        while(n > 0){
-//            n--;
-//            int last = n%26;
-//            sol = ((char)(last + 65)) + sol;
-//            n = n/26;
-//        }
-//        return sol;
-//}
 
     public static void main(String[] args) {
         //int input = 703;

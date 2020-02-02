efc488c5c41bd1c547d724a2aaa799946840a8b3
Milhail <vip.moneta95@mail.ru>
2020-02-02 13:18:01 +0700

    Valid Palindrome - finished

--- LeetCode/src/ru/mikhail/app/Valid_Palindrome_125.java
+++ LeetCode/src/ru/mikhail/app/Valid_Palindrome_125.java
@@ -1,28 +1,49 @@
 package ru.mikhail.app;
 
+/**
+ * Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+ *
+ * Note: For the purpose of this problem, we define empty string as valid palindrome.
+ *
+ * Example 1:
+ * Input: "A man, a plan, a canal: Panama"
+ * Output: true
+ *
+ * Example 2:
+ * Input: "race a car"
+ * Output: false
+ */
+
 public class Valid_Palindrome_125 {
     public boolean isPalindrome(String s) {
-        String helper = "";
+        s = s.toLowerCase();
         int left = 0;
-        int right = s.length()-1;
-        for (; left < right; left++) {
-            if (s.charAt(left) == ' ' || s.charAt(left) == ',' || s.charAt(left) == ':') {
-                left++;
+        int right = s.length() - 1;
+        while (left < right) {
+            while (left < right) {
+                if ((s.charAt(left) >= 'a' && s.charAt(left) <= 'z') || (s.charAt(left) >= '0' && s.charAt(left) <= '9')) {
+                    if ((s.charAt(right) >= 'a' && s.charAt(right) <= 'z') || (s.charAt(right) >= '0' && s.charAt(right) <= '9')) {
+                        if (s.charAt(left) != s.charAt(right)) {
+                            return false;
+                        } else {
+                            right--;
+                            left++;
+                        }
+                    } else {
+                        right--;
+                    }
+                } else {
+                    left++;
+                }
             }
-            if (s.charAt(right) == ' ' || s.charAt(right) == ',' || s.charAt(right) == ':') {
-                right--;
-            }
-            /*if (s.equalsIgnoreCase(s.charAt(i)) != s.equalsIgnoreCase(s.charAt(s.length() - 1))) {
-                return false;
-            }*/
-            right--;
         }
         return true;
     }
 
     public static void main(String[] args) {
         //String s = "A man, a plan, a canal: Panama";
-        String s = "race a car";
+        //String s = "race a car";
+        String s = "a.";
         Valid_Palindrome_125 function = new Valid_Palindrome_125();
         System.out.println(function.isPalindrome(s));
     }

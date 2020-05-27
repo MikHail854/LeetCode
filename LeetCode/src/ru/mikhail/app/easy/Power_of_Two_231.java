f4732092b97ffad8d09038988a9247e74ec8c61e
Milhail <vip.moneta95@mail.ru>
2020-05-27 15:13:54 +0700

    Power of Two - finished

--- LeetCode/src/ru/mikhail/app/easy/Power_of_Two_231.java
+++ LeetCode/src/ru/mikhail/app/easy/Power_of_Two_231.java
@@ -2,17 +2,17 @@ package ru.mikhail.app.easy;
 
 /**
  * Given an integer, write a function to determine if it is a power of two.
- *
+ * <p>
  * Example 1:
  * Input: 1
  * Output: true
  * Explanation: 2^0 = 1
-
+ * <p>
  * Example 2:
  * Input: 16
  * Output: true
  * Explanation: 2^4 = 16
-
+ * <p>
  * Example 3:
  * Input: 218
  * Output: false
@@ -35,7 +35,7 @@ public class Power_of_Two_231 {
             helper++;
         }
         int result = 1;
-        for (int i = 0; i < helper;i++){
+        for (int i = 0; i < helper; i++) {
             result = result * 2;
         }
         if (result == input) {

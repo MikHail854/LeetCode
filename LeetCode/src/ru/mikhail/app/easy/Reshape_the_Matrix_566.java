65a57f1f924314a8a33f4a2e1339b8670e665fe9
Milhail <vip.moneta95@mail.ru>
2020-06-09 14:57:25 +0700

    Reshape the Matrix - finished

---
+++ LeetCode/src/ru/mikhail/app/easy/Reshape_the_Matrix_566.java
@@ -0,0 +1,77 @@
+package ru.mikhail.app.easy;
+
+/**
+ * In MATLAB, there is a very useful function called 'reshape',
+ * which can reshape a matrix into a new one with different size but keep its original data.
+ *
+ * You're given a matrix represented by a two-dimensional array,
+ * and two positive integers r and c representing the row number
+ * and column number of the wanted reshaped matrix, respectively.
+ *
+ * The reshaped matrix need to be filled with all the elements of
+ * the original matrix in the same row-traversing order as they were.
+ *
+ * If the 'reshape' operation with given parameters is possible and legal,
+ * output the new reshaped matrix; Otherwise, output the original matrix.
+ *
+ * Example 1:
+ * Input:
+ * nums =
+ * [[1,2],
+ *  [3,4]]
+ * r = 1, c = 4
+ * Output:
+ * [[1,2,3,4]]
+ * Explanation:
+ * The row-traversing of nums is [1,2,3,4].
+ * The new reshaped matrix is a 1 * 4 matrix, fill it row by row by using the previous list.
+ *
+ * Example 2:
+ * Input:
+ * nums =
+ * [[1,2],
+ *  [3,4]]
+ * r = 2, c = 4
+ * Output:
+ * [[1,2],
+ *  [3,4]]
+ * Explanation:
+ * There is no way to reshape a 2 * 2 matrix to a 2 * 4 matrix. So output the original matrix.
+ *
+ * Note:
+ * 1) The height and width of the given matrix is in range [1, 100].
+ * 2) The given r and c are all positive.
+ */
+public class Reshape_the_Matrix_566 {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        if (nums.length == 0 || r * c != nums.length * nums[0].length)
+            return nums;
+        int[][] newNums = new int[r][c];
+        int rows = 0, cols = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[0].length; j++) {
+                newNums[rows][cols] = nums[i][j];
+                cols++;
+                if (cols == c) {
+                    rows++;
+                    cols = 0;
+                }
+            }
+        }
+        return newNums;
+    }
+
+    public static void main(String[] args) {
+        int[][] nums = {{1, 2}, {3, 4}};
+        int r = 1;
+        int c = 4;
+
+        Reshape_the_Matrix_566 function = new Reshape_the_Matrix_566();
+
+        for (int[] num : function.matrixReshape(nums, r, c)) {
+            for (int j = 0; j < nums[0].length; j++) {
+                System.out.println(num[j]);
+            }
+        }
+    }
+}

6ef96b86b818d440d9f4993370012e1881752063
Milhail <vip.moneta95@mail.ru>
2020-06-19 14:08:43 +0700

    First Bad Version - finished

--- LeetCode/src/ru/mikhail/app/easy/First_Bad_Version_278.java
+++ LeetCode/src/ru/mikhail/app/easy/First_Bad_Version_278.java
@@ -31,11 +31,11 @@ public class First_Bad_Version_278{
         int right = n;
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (isBadVersion(mid)) {
+            /*if (isBadVersion(mid)) {
                 right = mid;
             } else {
                 left = mid + 1;
-            }
+            }*/
         }
         return left;
     }

ef894e59bacb8652534050bafa3a278fbe3700a3
Milhail <vip.moneta95@mail.ru>
2020-06-20 13:56:46 +0700

    Symmetric Tree - finished

--- LeetCode/src/ru/mikhail/app/easy/Symmetric_Tree_101.java
+++ LeetCode/src/ru/mikhail/app/easy/Symmetric_Tree_101.java
@@ -1,25 +1,5 @@
 package ru.mikhail.app.easy;
 
-/**
- * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
- *
- * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
- *
- *     1
- *    / \
- *   2   2
- *  / \ / \
- * 3  4 4  3
- *
- *
- * But the following [1,2,2,null,3,null,3] is not:
- *
- *     1
- *    / \
- *   2   2
- *    \   \
- *    3    3
- */
 public class Symmetric_Tree_101 {
     public boolean isSymmetric(TreeNode root) {
         if (root == null)
@@ -28,12 +8,10 @@ public class Symmetric_Tree_101 {
     }
 
     public boolean isSymmetric(TreeNode left, TreeNode right) {
-        if (left == null || right == null) {
-            return left == right;
-        }
-        if (left.val != right.val) {
+        if (left == null || right == null)
+            return left ==  right;
+        if (left.val != right.val)
             return false;
-        }
         return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
     }
 

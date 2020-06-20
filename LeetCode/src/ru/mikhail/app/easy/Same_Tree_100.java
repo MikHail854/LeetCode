c050c9bc03ac33ca763e2cc0b963c5eb08f5d9e7
Milhail <vip.moneta95@mail.ru>
2020-06-20 12:57:20 +0700

    Same Tree - finished

--- LeetCode/src/ru/mikhail/app/easy/Same_Tree_100.java
+++ LeetCode/src/ru/mikhail/app/easy/Same_Tree_100.java
@@ -32,6 +32,7 @@ package ru.mikhail.app.easy;
  *         [1,2,1],   [1,1,2]
  * Output: false
  */
+
 public class Same_Tree_100 {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null)

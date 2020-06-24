e6b316e34106c3fe9310d987e61b2d75c2cbbe80
Milhail <vip.moneta95@mail.ru>
2020-06-24 11:05:12 +0700

    Power of Three - finished

--- LeetCode/src/ru/mikhail/app/easy/Power_of_Three_326.java
+++ LeetCode/src/ru/mikhail/app/easy/Power_of_Three_326.java
@@ -26,7 +26,7 @@ public class Power_of_Three_326 {
     public boolean isPowerOfThree(int n) {
         if (n == 1 || n == 3)
             return true;
-        while (n >= 3) {
+        while (n > 3) {
             if (n % 3 == 0) {
                 n = n / 3;
                 if (n == 3)
